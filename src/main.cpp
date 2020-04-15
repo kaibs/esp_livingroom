bf46a989f58753334f99bd72f82d2a6bf8942257
kaibs <kai.b.schneider@gmail.com>
2020-04-15 20:30:23 +0200

    added rf-switch

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,73 @@
 #include <ESP8266WiFi.h>
 #include "DHTesp.h"
 #include <PubSubClient.h>
+#include "RCSwitch.h"
 
 //include passwords
 #include "credentials.h"
-
 const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 const char* mqtt_server = MQTT_SERVER_IP;
 const char* user= MQTT_USER;
 const char* passw= MQTT_PASSWORD;
 
+//Wifi
 WiFiClient espLivingRoom;
 PubSubClient client(espLivingRoom);
 
+//mqtt messages
 #define MSG_BUFFER_SIZE	(50)
 char msg_temp[MSG_BUFFER_SIZE];
 char msg_humidity[MSG_BUFFER_SIZE];
+String receivedString;
 
+//FS1000A RF-Transmitter
+#define rfpin 15
+RCSwitch transmitter = RCSwitch();
 
 //DHT11 sensor
 #define dhtpin 2
-
 DHTesp dht;
 
-//functions
+//timer for sensor-publish
+unsigned long timer = 0;
+const long interval = 20000; //20 Sek.
 
+
+//-------------------------------------functions----------------------------------
+
+//callback for receiving MQTT
+void callback(char* topic, byte* payload, unsigned int length) {
+
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
+ if (strcmp(topic,"home/livingroom/ambilight/switch")==0){
+  
+  for (int i=0;i<length;i++) {
+
+    receivedString += (char)payload[i];
+  
+    if (receivedString == "ON"){
+      transmitter.sendTriState("F0FFFF0FFFFF");
+      delay(100);
+      Serial.println("ON");
+    }
+
+    if (receivedString == "OFF"){
+      transmitter.sendTriState("F0FFFF0F0000");
+      delay(100);
+      Serial.println("OFF");
+    }
+ }
+
+  receivedString = "";
+ }
+} 
+
+
+//reconnect for mqtt-broker
 void reconnect() {
  // Loop until we're reconnected
  while (!client.connected()) {
@@ -34,6 +76,9 @@ void reconnect() {
  // Attempt to connect
  if (client.connect("espLivingRoom", user, passw)) {
   Serial.println("connected");
+
+  //subscribe
+  client.subscribe("home/livingroom/ambilight/switch");
   
  } else {
   Serial.print("failed, rc=");
@@ -45,12 +90,16 @@ void reconnect() {
  }
 }
 
+
+//-------------------------------MAIN------------------------------------------------
 void setup() {
   
   Serial.begin(9600);
 
+  //DHT11
   dht.setup(dhtpin, DHTesp::DHT11);
 
+  //Wifi
   WiFi.hostname("espLivingRoom");
   WiFi.begin(ssid, password);
   
@@ -60,48 +109,46 @@ void setup() {
   }
   Serial.println("Connected to the WiFi network");
 
+  //mqtt
   client.setServer(mqtt_server, 1883);
-  //client.setCallback(callback);
+  client.setCallback(callback);
+
+  //rf
+  transmitter.enableTransmit(rfpin);
+  transmitter.setProtocol(1);
+  transmitter.setPulseLength(425);
 
 }
 
 void loop() {
 
+  //check mqtt-connection
   if (!client.connected()){
     reconnect();
   }
   client.loop();
 
-  delay(dht.getMinimumSamplingPeriod());
+  //timer
+  unsigned long currentTime = millis();
 
-  
-  float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
-  
-  Serial.print("humidity: "); 
-  Serial.print(humidity);
-  Serial.println(" %");
+  if (currentTime - timer >= interval){
 
-  Serial.print("temperature: ");
-  Serial.print(temperature);
-  Serial.println(" °C");
+    Serial.println("send data");
 
+    float humidity = dht.getHumidity();
+    float temperature = dht.getTemperature();
 
+    char helpval[8];
+    dtostrf(temperature, 6, 2, helpval);
+    snprintf (msg_temp, MSG_BUFFER_SIZE, helpval);
+    client.publish("home/livingroom/temperature", msg_temp);
+    delay(100);
 
+    dtostrf(humidity, 6, 2, helpval);
+    snprintf (msg_humidity, MSG_BUFFER_SIZE, helpval);
+    client.publish("home/livingroom/humidity", msg_humidity);
 
-  char helpval[8];
-  dtostrf(temperature, 6, 2, helpval);
-  snprintf (msg_temp, MSG_BUFFER_SIZE, helpval);
-  client.publish("home/livingroom/temperature", msg_temp);
-  delay(100);
+    timer = currentTime;
 
-
-  dtostrf(humidity, 6, 2, helpval);
-  snprintf (msg_humidity, MSG_BUFFER_SIZE, helpval);
-  client.publish("home/livingroom/humidity", msg_humidity);
-  
-
-  Serial.println("Message sent");
-  delay(2000);
-
+  }
 }
